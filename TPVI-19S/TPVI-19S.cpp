aceb345db17764c0d2de1c5983f60f6810d2832c
cyntrist <cyntrist@ucm.es>
2023-09-19 18:24:25 +0200

    en medio de clase

--- TPVI-19S/TPVI-19S.cpp
+++ TPVI-19S/TPVI-19S.cpp
@@ -1,19 +1,107 @@
-// TPVI-19S.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <fstream>
+#include "Date.h"
+#include <windows.h>
+#include <string>
+
+struct ListaCoches
+{
+    int tam;
+    int num;
+    Coche* c = new Coche[tam]
+};
+
+
+struct Coche 
+{
+    int id;
+    int precio;
+    std::string marca;
+    std::string modelo;
+
+    Coche(int i, int p, std::string ma, std::string mo) {
+        id = i;
+        precio = p;
+        marca = ma;
+        modelo = mo;
+    }
+
+    Coche() = default;
+};
+
+struct Alquiler
+{
+    int id;
+    int dias;
+    Date fecha;
+
+    Alquiler(int i, int d, Date f)
+    {
+        id = i;
+        dias = d;
+        fecha = f;
+    }
+
+    Alquiler() = default;
+};
+
+bool leerModelos() 
+{
+    bool abierto = false;
+    std::ifstream entrada("coches.txt");
+    if (entrada.is_open())
+    {
+        abierto = true;
+        int num = 0;
+        entrada >> num;
+        for (int i = 0; i < num; i++)
+        {
+            int id = 0;
+            int precio = 0;
+            std::string marca;
+            std::string modelo;
+            entrada >> id;
+            entrada >> precio;
+            entrada >> marca;
+            entrada >> modelo;
+            std::cout << "Coche " << i << ": " << id << " " << precio << " " << marca << " " << modelo << "\n";
+            Coche c = Coche(id, precio, marca, modelo);
+        }
+    }
+    return abierto;
+}
+
+int leerAlquileres()
+{
+    bool abierto = false;
+    std::ifstream entrada("rent.txt");
+    if (entrada.is_open())
+    {
+        abierto = true;
+        //...
+    }
+    return abierto;
+}
+
+int ordenarAlquileres()
+{
+    return 0;
+}
+
+int buscarCoche()
+{
+    return 0;
+}
+
+int mostrarAlquileres()
+{
+    return 0;
+}
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
     std::cout << "Hello World!\n";
+    leerModelos();
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
