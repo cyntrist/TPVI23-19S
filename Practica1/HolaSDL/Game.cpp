029dcc20e3258340f85aa2d50ad053ea882ab912
cyntrist <118978961+cyntrist@users.noreply.github.com>
2023-10-17 17:59:01 +0200

    base del game

---
+++ Practica1/HolaSDL/Game.cpp
@@ -0,0 +1,55 @@
+﻿#include "Game.h"
+#include <SDL.h>
+
+using namespace std;
+
+Game::Game() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (window == nullptr || renderer == nullptr)
+		throw "Error loading SDL window or renderer"s;
+	for (uint i = 0; i < NUM_TEXTURES; i++) {
+		// ...
+	}
+	// ...
+}
+Game::~Game() {
+	for (uint i = 0; i < NUM_TEXTURES; i++) delete textures[i];
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void Game::run()
+{
+	while (!exit)
+	{
+		handleEvents();
+		update();
+		render();
+	}
+}
+
+void Game::update()
+{
+	// ...
+}
+
+void Game::render() const
+{
+	SDL_RenderClear(renderer);
+	// ...
+	SDL_RenderPresent(renderer);
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event) && !exit)
+	{
+		if (event.type == SDL_QUIT) exit = true;
+		// ...
+	}
+}
---
+++ Practica1/HolaSDL/Game.h
@@ -0,0 +1,27 @@
+﻿#ifndef GAME_H
+#define GAME_H
+
+#include "SDL.h"
+#include "texture.h"
+
+using uint = unsigned int;
+
+constexpr uint WIN_WIDTH = 800;
+constexpr uint WIN_HEIGHT = 600;
+constexpr uint NUM_TEXTURES = 3;
+
+class Game {
+private:
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	bool exit = false;
+	Texture* textures[NUM_TEXTURES];
+public:
+	Game();
+	~Game();
+	void run();
+	void render() const;
+	void handleEvents();
+	void update();
+};
+#endif
