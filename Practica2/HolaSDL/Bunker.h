950bcfd5e6f3604283ad214b5ae55125082354a5
anonymous <dcanelad@ucm.es>
2023-11-27 18:30:58 +0100

    paz

--- Practica2/HolaSDL/Bunker.h
+++ Practica2/HolaSDL/Bunker.h
@@ -3,10 +3,13 @@
 #include "Vector2D.h"
 #include "SceneObject.h"
 
+class Game;
+
 class Bunker : public SceneObject
 {
 public:
 	Bunker() = default;
 	Bunker(const Point2D<double>& position, int lives, Texture* texture, Game* game);
 	void render() const override;
+	void update() override;
 };
