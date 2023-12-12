67c94b29a077ad9228323c4280cd385579c7af34
cyntrist <118978961+cyntrist@users.noreply.github.com>
2023-12-12 17:25:44 +0100

    material

--- Practica3/material/texture.h
+++ Practica3/material/texture.h
@@ -13,14 +13,14 @@ class Texture
 	SDL_Texture* texture = nullptr;
 	int width;
 	int height;
-	size_t nrows;
-	size_t ncolumns;
+	int nrows;
+	int ncolumns;
 	int frameWidth;
 	int frameHeight;
 
 public:
-	Texture(SDL_Renderer* renderer, const char* filename, size_t rows = 1, size_t columns = 1);
-	Texture(SDL_Renderer* renderer, SDL_Texture* texture, size_t rows = 1, size_t columns = 1);
+	Texture(SDL_Renderer* renderer, const char* filename, int rows = 1, int columns = 1);
+	Texture(SDL_Renderer* renderer, SDL_Texture* texture, int rows = 1, int columns = 1);
 	~Texture();
 
 	/// Width of a frame in pixels
@@ -51,6 +51,9 @@ public:
 	                 double angle,
 	                 const SDL_Point* center = nullptr,
 	                 SDL_RendererFlip flip = SDL_FLIP_NONE) const;
+
+	/// Render the whole picture to the given rectangle with modified color
+	void render(const SDL_Rect& target, SDL_Color color) const;
 };
 
 inline int
