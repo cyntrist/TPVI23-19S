3d26f1058903340ce63f1b9004bd1aee6e76d0c2
cyntrist <118978961+cyntrist@users.noreply.github.com>
2023-12-12 17:53:56 +0100

    Update GameState.h

--- Practica3/HolaSDL/GameState.h
+++ Practica3/HolaSDL/GameState.h
@@ -8,17 +8,20 @@
 class GameState
 {
 protected:
-    Game* game;
+    Game* game = nullptr;
     GameList<GameObject, true> gameObjects;
     std::list<EventHandler*> eventHandlers;
 public:
-    void update();
-    void render(); const
-    void handleEvent(const SDL_Event&);
-    void addEventListener();
-    void addObject();
-    void save(std::ostream&) const;
-    void hasDied(GameList<GameObject, true>::anchor);
+    virtual void update() = 0;
+    virtual void render() const = 0;
+    virtual void handleEvent(const SDL_Event&) = 0;
+    virtual void addEventListener();
+    virtual void addObject();
+    virtual void save(std::ostream&) const = 0;
+    virtual void hasDied(GameList<GameObject, true>::anchor) = 0;
+    virtual bool onEnter() = 0;
+	virtual bool onExit() = 0;
+	virtual std::string getStateID() const = 0;
     Game* getGame() const { return game; }
 };
 
