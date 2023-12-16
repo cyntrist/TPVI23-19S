ed34ca626d3da86e066198311377612601a0f940
David-104 <118064911+David-104@users.noreply.github.com>
2023-12-16 01:03:29 +0100

    eventhandler me estoy quedando dormido

--- Practica3/HolaSDL/EventHandler.h
+++ Practica3/HolaSDL/EventHandler.h
@@ -1,5 +1,9 @@
 #pragma once
+#include <SDL_events.h>
+
 class EventHandler
 {
+public:
+    virtual void handleEvent(const SDL_Event& event) = 0; //segun el enunciado solo es esto la clase (???
 };
 
